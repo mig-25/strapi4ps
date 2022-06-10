499f7ffd8814116dc8931c7f48d3ed7bf40c6b92
mig-25 <kd3180@hotmail.com>
2022-06-10 09:22:20 +0200

    add scheduler

--- config/plugins.js
+++ config/plugins.js
@@ -3,5 +3,12 @@ module.exports = ({ env }) => ({
     "import-export-entries": {
       enabled: true,
     },
+    scheduler: {
+      enabled: true,
+      config: {
+        model: "scheduler",
+      },
+    },
+    
     //...
   });
--- config/server.js
+++ config/server.js
@@ -1,6 +1,11 @@
+const cronTasks = require("@webbio/strapi-plugin-scheduler/cron-task");
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  cron: {
+    enabled: true,
+    tasks: cronTasks,
+  },
   app: {
     keys: env.array('APP_KEYS'),
   },
