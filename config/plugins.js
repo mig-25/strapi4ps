3893284dea72939a9dc1a4baf17cecac18f30fae
mig-25 <kd3180@hotmail.com>
2022-06-10 09:58:59 +0200

    add notes

--- config/plugins.js
+++ config/plugins.js
@@ -3,6 +3,9 @@ module.exports = ({ env }) => ({
     "import-export-entries": {
       enabled: true,
     },
+    'entity-notes': {
+      enabled: true,
+    },
     scheduler: {
       enabled: true,
       config: {
